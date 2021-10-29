eac7112a0b512cca3621f82e2c4643418a0fa0d3
Gina Im <eaim928@gmail.com>
2021-10-29 11:20:26 -0600

    got data loading

--- client/src/components/Criteria.js
+++ client/src/components/Criteria.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Criteria = () => {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (criteria) {
+        setData(criteria);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
-      <div>{/* filter through first five criteria and put in table */}</div>
+      <div>
+        {/* filter through first five criteria and put in table */}
+        {data && data.length > 0 && data.map((item) => <p>{item.mainText}</p>)}
+      </div>
       <div>{/* filter through last five criteria and put in table */}</div>
+      <div>{/* add line break and 100 percent */}</div>
       <div>{/* add non required criteria- hardcode in  */}</div>
     </>
   );
