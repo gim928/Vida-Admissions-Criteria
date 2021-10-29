d9a0d0e97b526ad3710322942505432c440c489d
Gina Im <eaim928@gmail.com>
2021-10-29 09:43:09 -0600

    added data and some comments

---
+++ client/src/components/Criteria.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Criteria = () => {
+  return (
+    <>
+      <div>{/* filter through first five criteria and put in table */}</div>
+      <div>{/* filter through last five criteria and put in table */}</div>
+      <div>{/* add non required criteria- hardcode in  */}</div>
+    </>
+  );
+};
+
+export default Criteria;
